d228b76f266b032aa90d53468f0690f9e24ab095
Akshay Pai S <akshaypai.s2001@gmail.com>
2026-01-09 09:07:59 +0530

    Initail commit: FastAPI setup

---
+++ app/main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, Response, status, HTTPException
+# from fastapi.params import Body
+from pydantic import BaseModel
+# from random import randrange
+
+class Post(BaseModel):
+    id : int
+    title : str
+    content : str
+    published : bool = False
+    rating : int | None = None
+
+
+app=FastAPI()
+my_posts : list=[{"tiltle":"dummy line", "content":"Dummy","id" : 0},{"title" : "First post", "content":"hi", "id":1}]
+
+def find_post(id:int):
+    for i in my_posts:
+        if i["id"]==id:
+            return i
+
+@app.get('/')
+def root():
+    return {'message':'Hello welcome hi sap'}
+
+@app.get('/posts')
+def get_posts():
+    return {'Posts' : my_posts}
+
+@app.post('/posts',status_code=status.HTTP_201_CREATED)
+def create_posts(payload : Post):
+    # post['id']=randrange(1,100000)
+    post=payload.model_dump()
+    my_posts.append(post)
+    return { 'post' : post}
+
+@app.get('/posts/latest')
+def get_latest():
+    post=my_posts[-1]
+    return {"last post" : post}
+
+@app.get('/posts/{id}')
+def get_post(id : int):
+    post=find_post(id)
+    if not post:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f'id {id} not found')
+        # response.status_code=status.HTTP_404_NOT_FOUND
+        # return {"Error" : f"The post with id : {id} not found"}
+    return {"post" : post}
+
+@app.delete('/posts/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    post=find_post(id)
+    if not post:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,f'id {id} not found')
+    my_posts.remove(post)
+
+
+@app.put('/posts/{id}')
+def update_post(id : int, post : Post):
+    p=find_post(id)
+    if not p:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,f'id {id} not found')
+    ind=my_posts.index(p)
+    post_dict=post.model_dump()
+    my_posts[ind]=post_dict
+    return {'updated' : post_dict}
