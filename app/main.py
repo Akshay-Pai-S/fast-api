25d8c58ffacb91f39bdd4042845e1e257d5b0e38
Akshay Pai S <akshaypai.s2001@gmail.com>
2026-01-18 13:43:50 +0530

    SQLAlchemy ORM Implimentation

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import Depends, FastAPI, Response, status, HTTPException
 # from fastapi.params import Body
 from pydantic import BaseModel
 # from random import randrange
 import psycopg
 from psycopg.rows import dict_row, namedtuple_row
 import time
+
+from app import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+from . import models
+
+models.Base.metadata.create_all(bind=engine)
+
 
 class Post(BaseModel):
     title : str
@@ -28,21 +36,31 @@ def find_post(id:int):
     cur.execute(t'select * from posts where id = {id}')
     return cur.fetchone()
 
+@app.get('/sqlalchemytest')
+def test_posts(db: Session = Depends(get_db)):
+    posts=db.query(models.Post).all()
+    return {"data" : posts}
+
 @app.get('/')
 def root():
     return {'message':'Hello welcome hi sap'}
 
 @app.get('/posts')
-def get_posts():
-    posts=cur.execute(t'select * from posts').fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    #posts=cur.execute(t'select * from posts').fetchall()
+    posts=db.query(models.Post).all()
     return {'Posts' : posts}
 
 @app.post('/posts',status_code=status.HTTP_201_CREATED)
-def create_posts(payload : Post):
-    post=cur.execute(
-        t'insert into posts (title, content, published) values ({payload.title}, {payload.content}, {payload.published}) returning *'
-        ).fetchone()
-    con.commit()
+def create_posts(payload : Post, db: Session = Depends(get_db)):
+    # post=cur.execute(
+    #     t'insert into posts (title, content, published) values ({payload.title}, {payload.content}, {payload.published}) returning *'
+    #     ).fetchone()
+    # con.commit()
+    post=models.Post(**payload.model_dump())
+    db.add(post)
+    db.commit()
+    db.refresh(post)
     return { 'post' : post}
 
 @app.get('/posts/latest/{limit}')
@@ -51,25 +69,34 @@ def get_latest(limit:int):
     return {"last post" : post}
 
 @app.get('/posts/{id}')
-def get_post(id : int):
-    post=find_post(id)
+def get_post(id : int,db: Session = Depends(get_db)):
+    # post=find_post(id)
+    post=db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status.HTTP_404_NOT_FOUND, f'id {id} not found')
     return {"post" : post}
 
 @app.delete('/posts/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id : int):
-    post=cur.execute(t'delete from posts where id= {id} returning *').fetchone()
-    if not post:
+def delete_post(id : int, db: Session = Depends(get_db)):
+    # post=cur.execute(t'delete from posts where id= {id} returning *').fetchone()
+    # con.commit()
+    post=db.query(models.Post).filter(models.Post.id == id)
+    if not post.first():
         raise HTTPException(status.HTTP_404_NOT_FOUND,f'id {id} not found')
-    con.commit()
+    post.delete(synchronize_session=False)
+    db.commit()
 
 @app.put('/posts/{id}')
-def update_post(id : int, payload : Post):
-    post=cur.execute(
-        t'update posts set title={payload.title} , content= {payload.content} , published={payload.published} where id={id} returning * '
-        ).fetchall()
+def update_post(id : int, payload : Post, db: Session = Depends(get_db)):
+    # post=cur.execute(
+    #     t'update posts set title={payload.title} , content= {payload.content} , published={payload.published} where id={id} returning * '
+    #     ).fetchall()
+    # con.commit()
+    post_query=db.query(models.Post).filter(models.Post.id == id)
+    post=post_query.first()
     if not post:
         raise HTTPException(status.HTTP_404_NOT_FOUND,f'id {id} not found')
-    con.commit()
+    post_query.update(payload.model_dump(), synchronize_session=False)
+    db.commit()
+    post=post_query.first()
     return {'updated' : post}
