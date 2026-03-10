739e50bf172d8bab9741c0dd44a2c6cc6ba94be1
Akshay Pai S <akshaypai.s2001@gmail.com>
2026-03-10 08:57:40 +0530

    Implimented CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .config import settings
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(
     title="FastAPI Social API",
@@ -9,6 +10,15 @@ app=FastAPI(
     debug=settings.debug,
 )
 
+origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
